f1493717447d2fa1bec6e6b78368b72d1ed4f58f
irenepapaspyratos <101578008+irenepapaspyratos@users.noreply.github.com>
2022-04-30 09:33:09 +0200

    Correction in pages/create.js

--- pages/create.js
+++ pages/create.js
@@ -1,5 +1,5 @@
 import { Typography } from '@mui/material';
-import CardCreateForm from '../src/components/cardCreateForm';
+import CardCreateForm from '../src/components/CardCreateForm';
 
 export default function Create() {
 	return (
