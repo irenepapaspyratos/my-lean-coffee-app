6f9a6e88b8f697628cde2c535d41e08e15eff2c3
Irene Papaspyratos <irene.papaspyratos@gmail.com>
2022-05-02 19:43:51 +0200

    Add hint to Card.js

--- src/models/Card.js
+++ src/models/Card.js
@@ -1,5 +1,5 @@
 import mongoose from 'mongoose';
-
+import User from './User'; // not used, but sometimes needed for ref bc loading-delay
 
 //const cardSchema = new mongoose.Schema({ content: String, name: String });
 const cardSchema = new mongoose.Schema({
@@ -7,7 +7,12 @@ const cardSchema = new mongoose.Schema({
 	user: { type: mongoose.Schema.Types.ObjectId, ref: 'User' },
 });
 
-
 const Card = mongoose.models.Card ?? mongoose.model('Card', cardSchema);
 
 export default Card;
+
+/* !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+Typischer FEHLER bei geänderten Models:
+Daten aus der DB mit alter Struktur werden als Fehler gelistet !
+=> Diese Daten müssen angepasst oder gelöscht werden.
+!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!*/
